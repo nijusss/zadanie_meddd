0c0365180bd9665a928673caf13079ed22a408e2
nijus <xd@gmail.com>
2022-03-29 12:30:16 +0200

    d

--- appointment.php
+++ appointment.php
@@ -13,7 +13,7 @@ if($q && $q->execute()) {
 if(isset($_REQUEST['firstName']) && isset($_REQUEST['lastName'])
             && $_REQUEST['phone']) {
     $q->prepare("INSERT INTO patient VALUES (NULL, ?, ?, ?");
-    $q->bind_param("sss", $_REQUEST['firstName'], $_REQUEST['lastName'], $_REQUEST), $_REQUEST['phone'];
+    $q->bind_param("sss", $_REQUEST['firstName'], $_REQUEST['lastName'], $_REQUEST), $_REQUEST['phone']);
     $q->execute();
     $patientId = $db->insert_id;
     $q->prepare("INSERT INTO patientappointment VALUES (NULL, ?, ?)");
